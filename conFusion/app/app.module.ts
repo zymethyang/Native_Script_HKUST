695bac32f2b9b10a61682db5680406c86f525741
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-22 19:04:15 +0700

    Fonts

--- conFusion/app/app.module.ts
+++ conFusion/app/app.module.ts
@@ -3,6 +3,7 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
+import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
 
 import { AppComponent } from "./app.component";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -33,7 +34,9 @@ import { baseURL } from './shared/baseurl';
         NativeScriptModule,
         AppRoutingModule,
         NativeScriptHttpModule,
-        NativeScriptUISideDrawerModule
+        NativeScriptUISideDrawerModule,
+        TNSFontIconModule.forRoot({
+            'fa': './fonts/font-awesome.min.css'})
     ],
     declarations: [
         AppComponent,
