37184a0037de3ec38b01026ca37ad86aa7f6c4aa
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-23 18:04:00 +0700

    Buttons

--- conFusion/app/dishdetail/dishdetail.component.ts
+++ conFusion/app/dishdetail/dishdetail.component.ts
@@ -5,10 +5,13 @@ import { DishService } from '../services/dish.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
 import 'rxjs/add/operator/switchMap';
+import { FavoriteService } from '../services/favorite.service';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import {Toasty} from 'nativescript-toasty';
 
 @Component({
   selector: 'app-dishdetail',
-    moduleId: module.id,
+  moduleId: module.id,
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.css']
 })
@@ -17,21 +20,42 @@ export class DishdetailComponent implements OnInit {
   dish: Dish;
   comment: Comment;
   errMess: string;
+  avgstars: string;
+  numcomments: number;
+  favorite: boolean = false;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
+    private favoriteservice: FavoriteService,
+    private fonticon: TNSFontIconService,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
 
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => this.dish = dish,
-          errmess => { this.dish = null; this.errMess = <any>errmess; });
+      .subscribe(dish => {
+        this.dish = dish;
+        this.favorite = this.favoriteservice.isFavorite(this.dish.id);
+        this.numcomments = this.dish.comments.length;
+
+        let total = 0;
+        this.dish.comments.forEach(comment => total += comment.rating);
+        this.avgstars = (total / this.numcomments).toFixed(2);
+      },
+      errmess => { this.dish = null; this.errMess = <any>errmess; });
   }
 
   goBack(): void {
     this.routerExtensions.back();
+  }
+  addToFavorites() {
+    if (!this.favorite) {
+      console.log('Adding to Favorites', this.dish.id);
+      this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+      const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+      toast.show();
+    }
   }
 }
--- conFusion/app/shared/baseurl.ts
+++ conFusion/app/shared/baseurl.ts
@@ -1,1 +1,1 @@
-export const baseURL = "http://192.168.0.111:3000/";
+export const baseURL = "http://192.168.0.106:3000/";
