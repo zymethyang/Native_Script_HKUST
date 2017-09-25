165ca035c1216355b2153969fe53c41dde3ae6a3
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-25 16:02:34 +0700

    Animations and Gestures Part 2

--- conFusion/app/dishdetail/dishdetail.component.ts
+++ conFusion/app/dishdetail/dishdetail.component.ts
@@ -11,6 +11,12 @@ import { Toasty } from 'nativescript-toasty';
 import * as dialogs from 'ui/dialogs';
 import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/modal-dialog';
 import { CommentComponent } from '../comment/comment.component';
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
 @Component({
   selector: 'app-dishdetail',
   moduleId: module.id,
@@ -25,7 +31,12 @@ export class DishdetailComponent implements OnInit {
   avgstars: string;
   numcomments: number;
   favorite: boolean = false;
-  result:any;
+  result: any;
+  showComments: boolean = false;
+
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
@@ -33,6 +44,7 @@ export class DishdetailComponent implements OnInit {
     private fonticon: TNSFontIconService,
     private modalService: ModalDialogService,
     private vcRef: ViewContainerRef,
+    private page: Page,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
@@ -90,5 +102,96 @@ export class DishdetailComponent implements OnInit {
       fullscreen: false,
     };
     return this.modalService.showModal(CommentComponent, options);
+  }
+
+  onSwipe(args: SwipeGestureEventData) {
+
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+
+  showAndHideComments() {
+    this.cardImage = <View>this.page.getViewById<View>("cardImage");
+    this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+    this.commentList = <View>this.page.getViewById<View>("commentList");
+
+    if (!this.showComments) {
+      this.animateUp();
+    }
+    else if (this.showComments) {
+      this.showComments = false;
+      this.animateDown();
+    }
+  }
+
+  animateUp() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+      target: this.cardImage,
+      scale: { x: 1, y: 0 },
+      translate: { x: 0, y: -200 },
+      opacity: 0,
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+      target: this.cardLayout,
+      backgroundColor: new Color("#ffc107"),
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+      this.showComments = true;
+    })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }
+
+  animateDown() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+      target: this.cardImage,
+      scale: { x: 1, y: 1 },
+      translate: { x: 0, y: 0 },
+      opacity: 1,
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+      target: this.cardLayout,
+      backgroundColor: new Color("#ffffff"),
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+    })
+      .catch((e) => {
+        console.log(e.message);
+      });
   }
 }
