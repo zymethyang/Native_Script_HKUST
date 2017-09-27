060deedfa3ce0354fcb8660763d99de367a5bfe5
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-27 16:23:32 +0700

    Social Sharing

--- conFusion/app/dishdetail/dishdetail.component.ts
+++ conFusion/app/dishdetail/dishdetail.component.ts
@@ -17,6 +17,9 @@ import { View } from "ui/core/view";
 import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
 import { Color } from 'color';
 import * as enums from "ui/enums";
+import * as SocialShare from "nativescript-social-share";
+import { ImageSource, fromUrl } from "image-source";
+
 @Component({
   selector: 'app-dishdetail',
   moduleId: module.id,
@@ -74,16 +77,19 @@ export class DishdetailComponent implements OnInit {
     }
   }
   displayActionDialog() {
+    
     dialogs.action({
       message: "Actions",
       cancelButtonText: "Cancel",
-      actions: ["Add to Favorites", "Add Comment"]
+      actions: ["Add to Favorites", "Add Comment", "Social Sharing"]
     }).then(result => {
       console.log("Dialog result: " + result);
       if (result == "Add to Favorites") {
         this.addToFavorites();
       } else if (result == "Add Comment") {
         this.showModal();
+      } else if (result === 'Social Sharing') {
+        this.socialShare();
       }
     });
   }
@@ -193,4 +199,16 @@ export class DishdetailComponent implements OnInit {
         console.log(e.message);
       });
   }
+  socialShare() {
+    let image: ImageSource;
+
+    fromUrl(this.BaseURL + this.dish.image)
+     .then((img: ImageSource) => {
+       image = img; 
+        SocialShare.shareImage(image, "How would you like to share this image?")
+      })
+     .catch(()=> { console.log('Error loading image'); });
+
+  }
+  
 }
